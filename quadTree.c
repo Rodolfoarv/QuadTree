2695f9f99e18376051269bc22d6399f3a2011c2a
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2015-09-13 22:31:16 -0500

    Added malloc statements

--- quadTree.c
+++ quadTree.c
@@ -5,19 +5,12 @@
  *           A01169701 Rodolfo Andrés Ramírez Valenzuela
  *           A01371743 Luis Eduardo Ballinas Aguilar
  *----------------------------------------------------------*/
-
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
+#define partition 3
 /********************************
 QuadTree
 *********************************/
-
 //Point struct
 typedef struct Points{
   int x;
@@ -40,19 +33,15 @@ typedef struct Nodes{
   unsigned int width;
   unsigned int height;
   char level[30];
-
   double mean;
   //Poblation of this node which will be represented by the points
   PointArray pointsArray;
-
-
 } Node;
-//Struct QuadTree
 
+//Struct QuadTree
 typedef struct QuadTree{
   Node *root;
 } QuadTree;
-
 
 /**********************
 QuadTree routines
@@ -64,11 +53,34 @@ void freeArray(PointArray *a);
 
 
 void insert(Node *node, PointArray *a, Point element) {
+
   PointArray pointsArray = node->pointsArray;
   printf("Used : %zd\n",pointsArray.used );
   if (a->used == a->size){
+    a->array[a->used++] = element;
     printf("Doing a partition\n" );
-    //if the node doesn't have any space then partition the Array
+
+    //Allocate memory for the childs
+    node->ne = (Node *)malloc(sizeof(Node));
+    node->nw = (Node *)malloc(sizeof(Node));
+    node->se = (Node *)malloc(sizeof(Node));
+    node->sw = (Node *)malloc(sizeof(Node));
+
+    initArray(&(node->ne->pointsArray),3);
+    initArray(&(node->nw->pointsArray),3);
+    initArray(&(node->se->pointsArray),3);
+    initArray(&(node->sw->pointsArray),3);
+
+
+    //Distribute the points between the children
+    int i;
+    for (i = 0; i < a->used; i++) {
+
+    }
+
+
+
+
   }else{
     a->array[a->used++] = element;
   }
@@ -91,6 +103,7 @@ void initArray(PointArray *a, size_t initialSize) {
   a->used = 0;
   a->size = initialSize;
 }
+
 /*
 void insertArray(PointArray *a, Point element) {
   if (a->used == a->size) {
@@ -128,6 +141,9 @@ int main(){
   Point pt3;
   pt3.x = 20;
   pt3.y = 4;
+  Point pt4;
+  pt4.x = 30;
+  pt4.y = 4;
 
   insert(&root, &root.pointsArray, pt1);
   insert(&root, &root.pointsArray, pt2);
@@ -137,20 +153,8 @@ int main(){
   printf("%d\n",root.pointsArray.array[1].x );
   printf("%d\n",root.pointsArray.array[2].x );
 
+  insert(&root, &root.pointsArray, pt3);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  printf("%d\n",root.pointsArray.array[3].x );
   return 1;
 }

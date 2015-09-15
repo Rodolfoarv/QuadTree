d8d225934aba0b325d96fbb873c14fc8e5066ff2
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2015-09-15 01:37:01 -0500

    Added getMean, getPointsList routines

--- quadTree.c
+++ quadTree.c
@@ -40,6 +40,7 @@ typedef struct Nodes{
   int lowerBound_Y;
   //Poblation of this node which will be represented by the points
   PointArray pointsArray;
+  PointArray pointsArrayMean; //Array which will be used to calculate the mean and all the points contained in that node
 } Node;
 
 //Struct QuadTree
@@ -56,13 +57,15 @@ int size (Point arr[]);
 void initArray(PointArray *a, size_t initialSize);
 void freeArray(PointArray *a);
 int getQuadrant(Node *node, Point *element);
-
+void getPointsList(Node *node); //Consulta de datos (local)
+void getNodeMean(Node *node);
 
 void insert(Node *node, PointArray *a, Point element) {
   //printf(" Lower bound: %d Upper bound: %d con el punto %d %d ", node->lowerBound_X, node->upperBound_X, element.x, element.y);
   //printf("Size: %zd Used: %zd\n", a->size, a->used );
-  /*
+  node->pointsArrayMean.array[node->pointsArrayMean.used++] = element;
   if (a-> size == 0){
+    printf("Inserting point x:%d y:%d \n", element.x, element.y );
     int quadrant = getQuadrant(node, &element);
     if (quadrant == 1){
       insert(node->ne,&(node->ne->pointsArray),element);
@@ -74,8 +77,8 @@ void insert(Node *node, PointArray *a, Point element) {
       insert(node->nw,&(node->nw->pointsArray),element);
 
     }
-  }*/ if (a->used == a->size-1){
-    printf("Divido!\n" );
+  } if (a->used == a->size-1){
+    printf("PARTITIONING INTO CHILDS!\n" );
     subdivision(node, a, element);
     freeArray(a);
   }else{ //Insert into the original node
@@ -99,6 +102,11 @@ void subdivision (Node *node, PointArray *a, Point element){
   initArray(&(node->nw->pointsArray),PARTITION);
   initArray(&(node->se->pointsArray),PARTITION);
   initArray(&(node->sw->pointsArray),PARTITION);
+  initArray(&(node->ne->pointsArrayMean),PARTITION);
+  initArray(&(node->nw->pointsArrayMean),PARTITION);
+  initArray(&(node->se->pointsArrayMean),PARTITION);
+  initArray(&(node->sw->pointsArrayMean),PARTITION);
+
 
   //Set the width and height for the childs
   //NORTH EAST
@@ -196,6 +204,33 @@ int getQuadrant(Node *node, Point *element){
   return element->quadrant;
 
 }
+
+void getPointsList(Node *node){
+  int i;
+  printf("******** List of points inside this Node ********* \n" );
+  printf("This node has a total of %zd Points\n", node->pointsArrayMean.used  );
+  for (i = 0; i < node->pointsArrayMean.used; i++){
+    printf("(%d, %d)\n",node->pointsArrayMean.array[i].x,node->pointsArrayMean.array[i].y);
+  }
+}
+
+void getNodeMean(Node *node){
+  if (node->pointsArrayMean.used == 0){
+    printf("This node doesn't have any values stored yet\n" );
+  }else{
+    int i;
+    double x,y;
+    for (i = 0; i < node->pointsArrayMean.used; i++){
+      printf("Point :%d , %d\n",node->pointsArrayMean.array[i].x,node->pointsArrayMean.array[i].y );
+      x+= node->pointsArrayMean.array[i].x;
+      y+= node->pointsArrayMean.array[i].y;
+    }
+    x = x / node->pointsArrayMean.used;
+    y = y / node->pointsArrayMean.used;
+    printf("The node mean is (%f, %f)\n",x,y );
+  }
+
+}
 /**********************
 Dynamic Point Array Structure
 ***********************/
@@ -212,25 +247,11 @@ void initArray(PointArray *a, size_t initialSize) {
   a->size = initialSize+1;
 }
 
-/*
-void insertArray(PointArray *a, Point element) {
-  if (a->used == a->size) {
-    a->size *= 2;
-    a->array = (Point *)realloc(a->array, a->size * sizeof(Point));
-  }
-  a->array[a->used++] = element;
-}
-*/
-
 void freeArray(PointArray *a) {
-  printf("Im free modafuckas\n" );
   free(a->array);
   //a->array = NULL;
   a->used = a->size = 0;
-
 }
-
-
 
 int main(){
   //Create the initial QuadTree Structure
@@ -246,33 +267,41 @@ int main(){
 
   //quadTree.root = &root;
   initArray(&root.pointsArray,PARTITION);
+  initArray(&root.pointsArrayMean,3000);
 
   Point pt1; pt1.x = 12 ; pt1.y = 15;
   Point pt2; pt2.x = 11; pt2.y = 20;
   Point pt3; pt3.x = 15; pt3.y = 11;
   Point pt4; pt4.x = 20; pt4.y = 20;
-
-
-/*
-
   Point pt5; pt5.x = 10; pt5.y = 5;
   Point pt6; pt6.x = 3; pt6.y = 4;
-  Point pt7; pt7.x = 11; pt7.y = 20;
-  Point pt8; pt8.x = 5; pt8.y = 5;
-  Point pt11; pt11.x = 6; pt11.y = 6;
-  Point pt12; pt12.x = 7; pt12.y = 7;
-  //Point pt13; pt13.x = 8; pt13.y = 8;
-  //Point pt9; pt9.x = 20; pt9.y = 20;
-  //Point pt10; pt10.x = 10; pt10.y = 5;
+  Point pt7; pt7.x = 12; pt7.y = 20;
+
+/*
+Point pt8; pt8.x = 5; pt8.y = 5;
+Point pt9; pt9.x = 20; pt9.y = 20;
+Point pt10; pt10.x = 10; pt10.y = 5;
+Point pt11; pt11.x = 6; pt11.y = 6;
+Point pt12; pt12.x = 7; pt12.y = 7;
+Point pt13; pt13.x = 8; pt13.y = 8;
+
 */
   insert(&root, &root.pointsArray, pt1);
   insert(&root, &root.pointsArray, pt2);
   insert(&root, &root.pointsArray, pt3);
   insert(&root, &root.pointsArray, pt4);
-  printf("%d\n",root.ne->sw->pointsArray.array[0].x );
-/*
   insert(&root, &root.pointsArray, pt5);
-  //insert(&root, &root.pointsArray, pt6);
+  insert(&root, &root.pointsArray, pt6);
+  insert(&root, &root.pointsArray, pt7);
+
+
+  //printf("%d\n",root.ne->sw->pointsArray.array[0].y );
+  printf("%d\n",root.ne->nw->pointsArray.array[0].x );
+  getNodeMean(root.ne->ne);
+  getPointsList(root.ne);
+/*
+
+
   insert(&root, &root.pointsArray, pt7);
   insert(&root, &root.pointsArray, pt8);
   insert(&root, &root.pointsArray, pt11);
